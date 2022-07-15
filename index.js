49672b745a5fb7a57a0a2945aff7e05b47adeb57
EC2 Default User <ec2-user@ip-172-31-21-249.ap-northeast-1.compute.internal>
2022-07-15 04:23:26 +0000

    first

---
+++ index.js
@@ -0,0 +1,45 @@
+const Loading = require('mofron-comp-sploading');
+const Fade    = require('mofron-effect-fadepack');
+const Rotate  = require('mofron-effect-rotate');
+const ConfArg = mofron.class.ConfArg;
+let loading   = new Loading({ effect: new Fade(500) });
+
+module.exports = {
+    show: (msg) => {
+        try {
+	    let r_chd = mofron.root[0].child();
+	    let is_exist = false
+	    for (let ridx in r_chd) {
+                if (r_chd[ridx].id() === loading.id()) {
+                    is_exist = true;
+                    break;
+		}
+	    }
+	    if (false === is_exist) {
+                mofron.root[0].child(loading);
+	    }
+	    loading.text(msg);
+	    loading.visible(true);
+	} catch (e) {
+            console.error(e.stack);
+	    throw e;
+	}
+    },
+    hide: () => {
+        try {
+            loading.visible(false);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    },
+    config: (prm) => {
+        try {
+            loading.config(prm);
+	} catch (e) {
+            console.error(e.stack);
+	                throw e;
+
+	}
+    }
+};
